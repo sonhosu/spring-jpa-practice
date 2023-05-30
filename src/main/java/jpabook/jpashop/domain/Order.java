8b2b406fd143ccb4749cf241b3a004461b52420d
DESKTOP-JURDD5A\ONLINE-DEV02 <ghtn9398@nate.com>
2023-05-30 17:56:07 +0900

    상품 주문 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,29 +1,94 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
 @Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id@GeneratedValue
     @Column(name = "order_id")
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
 
-    @OneToMany(mappedBy ="order")
-    private List<OrderItem> orderItems = new ArrayList<>();
-
-    @OneToOne
-    @JoinColumn(name = "delivery_id")
-    private Delivery delivery;
+    @Column
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus; // 주문상태
+    private OrderStatus status; // 주문상태
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy ="order" , cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    //연관관계 메서드
+
+    public void setMember(Member member){
+
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    // order 생성메서드
+    public static  Order createOrder(Member member , Delivery delivery , OrderItem... orderItems){
+        Order order= new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate( LocalDateTime.now());
+        return order;
+    }
+
+
+    // 비지니스 로직
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품운 취소가 불가능합니다");
+        }
+
+        this.setStatus(OrderStatus.CANCLE);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    public  int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
